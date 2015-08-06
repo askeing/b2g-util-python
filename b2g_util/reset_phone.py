a18315303c795f68e7477364e585d28ebe191b36
askeing <askeing@gmail.com>
2015-08-06 18:42:36 +0800

    Modified reset_phone

--- b2g_util/reset_phone.py
+++ b2g_util/reset_phone.py
@@ -5,9 +5,14 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import os
+import logging
 import argparse
 from argparse import ArgumentDefaultsHelpFormatter
+from util.adb_helper import AdbHelper
 from util.adb_helper import AdbWrapper
+
+
+logger = logging.getLogger(__name__)
 
 
 class PhoneReseter(object):
@@ -15,75 +20,59 @@ class PhoneReseter(object):
         self.arg_parser = argparse.ArgumentParser(description='Reset Firefox OS Phone.',
                                                   formatter_class=ArgumentDefaultsHelpFormatter)
         self.arg_parser.add_argument('-s', '--serial', action='store', dest='serial', default=None, help='Directs command to the device or emulator with the given serial number. Overrides ANDROID_SERIAL environment variable.')
+        self.arg_parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', default=False, help='Turn on verbose output, with all the debug logger.')
         self.args = self.arg_parser.parse_args()
+        # setup the logging config
+        if self.args.verbose is True:
+            verbose_formatter = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+            logging.basicConfig(level=logging.DEBUG, format=verbose_formatter)
+        else:
+            formatter = '%(levelname)s: %(message)s'
+            logging.basicConfig(level=logging.INFO, format=formatter)
+        self.check_adb()
 
-    def print_log(self, msg):
-        print '###', msg
+    def check_adb(self):
+        # check adb
+        if not AdbWrapper.has_adb():
+            raise Exception('There is no "adb" in your environment PATH.')
 
     def reset_phone(self, serial=None):
-        self.print_log('Starting to Reset Firefox OS Phone...')
+        # checking the adb root for backup/restore
+        if not AdbWrapper.adb_root(serial=serial):
+            raise Exception('No root permission for backup and resotre.')
+        # starting to reset
+        logger.info('Starting to Reset Firefox OS Phone...')
         AdbWrapper.adb_shell('rm -r /cache/*', serial=serial)
         AdbWrapper.adb_shell('mkdir /cache/recovery', serial=serial)
         AdbWrapper.adb_shell('echo "--wipe_data" > /cache/recovery/command', serial=serial)
         AdbWrapper.adb_shell('reboot recovery', serial=serial)
-        self.print_log('Reset Firefox OS Phone done.')
+        logger.info('Reset Firefox OS Phone done.')
 
     def run(self):
         devices = AdbWrapper.adb_devices()
 
         if len(devices) == 0:
-            print 'No device.'
-            exit(1)
+            raise Exception('No device.')
         elif len(devices) >= 1:
-            # has --serial, then skip ANDROID_SERIAL
-            # then reset one device by --serial
-            if (self.args.serial is not None):
-                if self.args.serial in devices:
-                    serial = self.args.serial
-                    print 'Serial: {0} (State: {1})'.format(serial, devices[serial])
-                    ret = self.reset_phone(serial=serial)
+            final_serial = AdbHelper.get_serial(self.args.serial)
+            if final_serial is None:
+                if len(devices) == 1:
+                    logger.debug('No serial, and only one device')
+                    self.reset_phone(serial=final_serial)
                 else:
-                    print 'Can not found {0}.\nDevices:'.format(self.args.serial)
-                    for device, state in devices.items():
-                        print 'Serial: {0} (State: {1})'.format(device, state)
-                    exit(1)
-            # no --serial, but has ANDROID_SERIAL
-            # then reset one device by ANDROID_SERIAL
-            elif (self.args.serial is None) and ('ANDROID_SERIAL' in os.environ):
-                if os.environ['ANDROID_SERIAL'] in devices:
-                    serial = os.environ['ANDROID_SERIAL']
-                    print 'Serial: {0} (State: {1})'.format(serial, devices[serial])
-                    ret = self.reset_phone(serial=serial)
-                else:
-                    print 'Can not found {0}.\nDevices:'.format(os.environ['ANDROID_SERIAL'])
-                    for device, state in devices.items():
-                        print 'Serial: {0} (State: {1})'.format(device, state)
-                    exit(1)
-            # no --serial, no ANDROID_SERIAL
-            # more than ONE devices, then reset NO devices
-            # only ONE devices, then reset this device
-            if (self.args.serial is None) and (not 'ANDROID_SERIAL' in os.environ):
-                if len(devices) > 1:
-                    print 'More than one device.'
-                    print 'Please specify ANDROID_SERIAL by "--serial" option.\n'
-                    device_info_list = []
-                    for device, state in devices.items():
-                        print 'Serial: {0} (State: {1})'.format(device, state)
-                    exit(1)
-                elif len(devices) == 1:
-                    for device, state in devices.items():
-                        print 'Serial: {0} (State: {1})'.format(device, state)
-                        ret = self.reset_phone(serial=device)
+                    logger.debug('No serial, but there are more than one device')
+                    raise Exception('Please specify the device by --serial option.')
+            else:
+                print('Serial: {0} (State: {1})'.format(final_serial, devices[final_serial]))
+                self.reset_phone(serial=final_serial)
 
 
 def main():
-    if not AdbWrapper.has_adb():
-        print 'There is no "adb" in your environment PATH.'
+    try:
+        PhoneReseter().run()
+    except Exception as e:
+        logger.error(e)
         exit(1)
-
-    my_app = PhoneReseter()
-    my_app.run()
-
 
 if __name__ == '__main__':
     main()
